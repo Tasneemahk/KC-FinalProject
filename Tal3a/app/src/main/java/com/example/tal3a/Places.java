4843e3ee086a6da50d71bb185198e8a94c299941
Tasneemahk <tasneemahk824@gmail.com>
2022-09-07 21:28:59 +0300

    deleted app

--- Tal3a/app/src/main/java/com/example/tal3a/Places.java
+++
@@ -1,7 +0,0 @@
-package com.example.tal3a;
-
-public class Places {
-    private String placeName;
-    private String description;
-
-}
--- Tal3a/app/src/main/java/com/example/tal3a/Tips.java
+++
@@ -1,7 +0,0 @@
-package com.example.tal3a;
-
-public class Tips {
-    private int tipImage;
-    private String tip;
-
-}
